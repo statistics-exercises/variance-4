3637dfdcee40fc81146b49853af47d258173b1c1
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-29 19:28:58 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np 
+
+def sample_mean(m) : 
+  # Your code to calculate a  sample mean from m 
+  # uniform random variables between 0 and 1 goes here
+  
+  
+def variance(n,m) : 
+  # Your code to estimate the variance for a set of n 
+  # sample means, which are each computed from m 
+  # uniform random variables between 0 and 1 goes here.
+  
+  
+
+# This code shouldn't need to be modified.  It will output three 
+# estimates of the variance of a sample mean computed from 
+# 10 uniform random variables.
+print( variance( 100, 10), variance( 100, 10), variance( 100, 10) )
